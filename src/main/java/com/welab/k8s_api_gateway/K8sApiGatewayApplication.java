14e48b3078f1097e2b50e3f7922c6e6afdeacd78
rlaalsdn0421 <85922540+rlaalsdn0421@users.noreply.github.com>
2025-06-11 16:54:15 +0900

    ㄴㅁㅇ

--- src/main/java/com/welab/k8s_api_gateway/K8sApiGatewayApplication.java
+++ src/main/java/com/welab/k8s_api_gateway/K8sApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.welab.k8s_api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableFeignClients
 @SpringBootApplication
 public class K8sApiGatewayApplication {
 
